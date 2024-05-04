12b875b8fcd7b63f97462e405283ec1804f3d1dd
Jose Angel <jgallego@peopleofthe.cloud>
2024-05-04 11:49:24 +0200

    trasdc

--- traci.py
+++ traci.py
@@ -1,2 +1,14 @@
 import traci
 
+# Connect to SUMO simulation
+traci.start(["sumo", "-c", "net/flujoRutas.sumocfg"])
+
+# Simulation loop
+step = 0
+while step < 1000:
+    traci.simulationStep()
+    # Your simulation logic here
+    step += 1
+
+# Close TraCI connection
+traci.close()
